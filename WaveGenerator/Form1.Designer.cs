30f6bc329dd452339fd1a5752365c5086744d438
Kris Janssen <kris.janssen@gmail.com>
2015-11-05 08:55:08 +0100

    Added a simple wave generator for single axis galvo

---
+++ WaveGenerator/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace WaveGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnON = new System.Windows.Forms.Button();
+            this.btnOFF = new System.Windows.Forms.Button();
+            this.lblSTATUS = new System.Windows.Forms.Label();
+            this.btnSTART = new System.Windows.Forms.Button();
+            this.btnSTOP = new System.Windows.Forms.Button();
+            this.btnMOVE = new System.Windows.Forms.Button();
+            this.txtMOVE = new System.Windows.Forms.TextBox();
+            this.lblPOS = new System.Windows.Forms.Label();
+            this.txtPIXELS = new System.Windows.Forms.TextBox();
+            this.txtTPL = new System.Windows.Forms.TextBox();
+            this.lblPIXELS = new System.Windows.Forms.Label();
+            this.lblTPL = new System.Windows.Forms.Label();
+            this.workerMove = new System.ComponentModel.BackgroundWorker();
+            this.workerScan = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // btnON
+            // 
+            this.btnON.Location = new System.Drawing.Point(13, 13);
+            this.btnON.Name = "btnON";
+            this.btnON.Size = new System.Drawing.Size(75, 23);
+            this.btnON.TabIndex = 0;
+            this.btnON.Text = "ON";
+            this.btnON.UseVisualStyleBackColor = true;
+            this.btnON.Click += new System.EventHandler(this.btnON_Click);
+            // 
+            // btnOFF
+            // 
+            this.btnOFF.Location = new System.Drawing.Point(13, 43);
+            this.btnOFF.Name = "btnOFF";
+            this.btnOFF.Size = new System.Drawing.Size(75, 23);
+            this.btnOFF.TabIndex = 1;
+            this.btnOFF.Text = "OFF";
+            this.btnOFF.UseVisualStyleBackColor = true;
+            this.btnOFF.Click += new System.EventHandler(this.btnOFF_Click);
+            // 
+            // lblSTATUS
+            // 
+            this.lblSTATUS.AutoSize = true;
+            this.lblSTATUS.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSTATUS.ForeColor = System.Drawing.Color.Red;
+            this.lblSTATUS.Location = new System.Drawing.Point(117, 26);
+            this.lblSTATUS.Name = "lblSTATUS";
+            this.lblSTATUS.Size = new System.Drawing.Size(57, 25);
+            this.lblSTATUS.TabIndex = 2;
+            this.lblSTATUS.Text = "OFF";
+            // 
+            // btnSTART
+            // 
+            this.btnSTART.Location = new System.Drawing.Point(197, 13);
+            this.btnSTART.Name = "btnSTART";
+            this.btnSTART.Size = new System.Drawing.Size(75, 23);
+            this.btnSTART.TabIndex = 3;
+            this.btnSTART.Text = "START";
+            this.btnSTART.UseVisualStyleBackColor = true;
+            this.btnSTART.Click += new System.EventHandler(this.btnSTART_Click);
+            // 
+            // btnSTOP
+            // 
+            this.btnSTOP.Location = new System.Drawing.Point(197, 43);
+            this.btnSTOP.Name = "btnSTOP";
+            this.btnSTOP.Size = new System.Drawing.Size(75, 23);
+            this.btnSTOP.TabIndex = 4;
+            this.btnSTOP.Text = "STOP";
+            this.btnSTOP.UseVisualStyleBackColor = true;
+            this.btnSTOP.Click += new System.EventHandler(this.btnSTOP_Click);
+            // 
+            // btnMOVE
+            // 
+            this.btnMOVE.Location = new System.Drawing.Point(13, 114);
+            this.btnMOVE.Name = "btnMOVE";
+            this.btnMOVE.Size = new System.Drawing.Size(75, 23);
+            this.btnMOVE.TabIndex = 5;
+            this.btnMOVE.Text = "Move";
+            this.btnMOVE.UseVisualStyleBackColor = true;
+            this.btnMOVE.Click += new System.EventHandler(this.btnMOVE_Click);
+            // 
+            // txtMOVE
+            // 
+            this.txtMOVE.Location = new System.Drawing.Point(172, 116);
+            this.txtMOVE.Name = "txtMOVE";
+            this.txtMOVE.Size = new System.Drawing.Size(100, 20);
+            this.txtMOVE.TabIndex = 6;
+            this.txtMOVE.Text = "0.0";
+            // 
+            // lblPOS
+            // 
+            this.lblPOS.AutoSize = true;
+            this.lblPOS.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPOS.Location = new System.Drawing.Point(133, 77);
+            this.lblPOS.Name = "lblPOS";
+            this.lblPOS.Size = new System.Drawing.Size(25, 25);
+            this.lblPOS.TabIndex = 7;
+            this.lblPOS.Text = "0";
+            // 
+            // txtPIXELS
+            // 
+            this.txtPIXELS.Location = new System.Drawing.Point(172, 143);
+            this.txtPIXELS.Name = "txtPIXELS";
+            this.txtPIXELS.Size = new System.Drawing.Size(100, 20);
+            this.txtPIXELS.TabIndex = 8;
+            this.txtPIXELS.Text = "1000";
+            // 
+            // txtTPL
+            // 
+            this.txtTPL.Location = new System.Drawing.Point(172, 170);
+            this.txtTPL.Name = "txtTPL";
+            this.txtTPL.Size = new System.Drawing.Size(100, 20);
+            this.txtTPL.TabIndex = 9;
+            this.txtTPL.Text = "2.0";
+            // 
+            // lblPIXELS
+            // 
+            this.lblPIXELS.AutoSize = true;
+            this.lblPIXELS.Location = new System.Drawing.Point(82, 146);
+            this.lblPIXELS.Name = "lblPIXELS";
+            this.lblPIXELS.Size = new System.Drawing.Size(84, 13);
+            this.lblPIXELS.TabIndex = 10;
+            this.lblPIXELS.Text = "Samples per line";
+            this.lblPIXELS.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lblTPL
+            // 
+            this.lblTPL.AutoSize = true;
+            this.lblTPL.Location = new System.Drawing.Point(77, 173);
+            this.lblTPL.Name = "lblTPL";
+            this.lblTPL.Size = new System.Drawing.Size(89, 13);
+            this.lblTPL.TabIndex = 11;
+            this.lblTPL.Text = "Time per line (ms)";
+            this.lblTPL.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // workerMove
+            // 
+            this.workerMove.DoWork += new System.ComponentModel.DoWorkEventHandler(this.workerMove_DoWork);
+            // 
+            // workerScan
+            // 
+            this.workerScan.DoWork += new System.ComponentModel.DoWorkEventHandler(this.workerScan_DoWork);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.lblTPL);
+            this.Controls.Add(this.lblPIXELS);
+            this.Controls.Add(this.txtTPL);
+            this.Controls.Add(this.txtPIXELS);
+            this.Controls.Add(this.lblPOS);
+            this.Controls.Add(this.txtMOVE);
+            this.Controls.Add(this.btnMOVE);
+            this.Controls.Add(this.btnSTOP);
+            this.Controls.Add(this.btnSTART);
+            this.Controls.Add(this.lblSTATUS);
+            this.Controls.Add(this.btnOFF);
+            this.Controls.Add(this.btnON);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnON;
+        private System.Windows.Forms.Button btnOFF;
+        private System.Windows.Forms.Label lblSTATUS;
+        private System.Windows.Forms.Button btnSTART;
+        private System.Windows.Forms.Button btnSTOP;
+        private System.Windows.Forms.Button btnMOVE;
+        private System.Windows.Forms.TextBox txtMOVE;
+        private System.Windows.Forms.Label lblPOS;
+        private System.Windows.Forms.TextBox txtPIXELS;
+        private System.Windows.Forms.TextBox txtTPL;
+        private System.Windows.Forms.Label lblPIXELS;
+        private System.Windows.Forms.Label lblTPL;
+        private System.ComponentModel.BackgroundWorker workerMove;
+        private System.ComponentModel.BackgroundWorker workerScan;
+    }
+}
+
---
+++ WaveGenerator/Form1.cs
@@ -0,0 +1,276 @@
+﻿using SIS.Forms;
+using SIS.ScanModes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WaveGenerator
+{
+    public partial class Form1 : Form
+    {
+        private SIS.Hardware.IPiezoStage m_Stage;
+        private SIS.Forms.TrajectoryPlotForm m_frmTrajectoryForm = new TrajectoryPlotForm();
+
+        // Delegate involved in handling cross thread passing of data from Hardware to UI.
+        private delegate void UIUpdateDelegate();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Create a suitable stage.
+            this.m_Stage = SIS.Hardware.NIAnalogStage.Instance;
+
+            // Hook up EventHandler methods to the events of the stage.
+            this.m_Stage.PositionChanged += new EventHandler(m_Stage_PositionChanged);
+            this.m_Stage.ErrorOccurred += new EventHandler(m_Stage_ErrorOccurred);
+            this.m_Stage.EngagedChanged += new EventHandler(m_Stage_EngagedChanged);
+        }
+
+        void m_Stage_EngagedChanged(object sender, EventArgs e)
+        {
+            // The text indicating stage status should always be updated.
+            this.lblSTATUS.Text = this.m_Stage.IsInitialized.ToString();
+        }
+
+        void m_Stage_ErrorOccurred(object __oSender, EventArgs __evargsE)
+        {
+            MessageBox.Show(m_Stage.CurrentError);
+        }
+
+        void m_Stage_PositionChanged(object __oSender, EventArgs __evargsE)
+        {
+            if (InvokeRequired)
+            {
+                Invoke(new UIUpdateDelegate(this.UpdateUI));
+            }
+            else
+            {
+                this.UpdateUI();
+            }
+        }
+
+        private void UpdateUI()
+        {
+            // Update the UI with the current voltage to stage.
+            this.lblPOS.Text = this.m_Stage.XPosition.ToString();
+
+            // Process any events that might be waiting.
+            Application.DoEvents();
+        }
+
+        // Called to re-enable controls after a scan.
+        private void EnableCtrls()
+        {
+            // Disable the Scan button because validation is always necessary before scanning can start.
+            this.btnMOVE.Enabled = true;
+            this.btnOFF.Enabled = true;
+            this.btnON.Enabled = true;
+            this.btnSTOP.Enabled = true;
+            this.btnSTART.Enabled = true;
+        }
+
+        // Called to enable controls during a scan in progress.
+        private void DisableCtrls()
+        {
+            // Disable the Scan button.
+            this.btnMOVE.Enabled = false;
+            this.btnOFF.Enabled = false;
+            this.btnON.Enabled = false;
+            this.btnSTOP.Enabled = true;
+            this.btnSTART.Enabled = false;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                if (m_Stage.IsInitialized)
+                {
+                    throw new SIS.Hardware.StageNotReleasedException("The stage was not released! Please use stage control to turn it off!");
+                }
+                this.m_Stage.PositionChanged -= new EventHandler(m_Stage_PositionChanged);
+                this.m_Stage.ErrorOccurred -= new EventHandler(m_Stage_ErrorOccurred);
+                this.m_Stage.EngagedChanged -= new EventHandler(m_Stage_EngagedChanged);
+            }
+
+            catch (SIS.Hardware.StageNotReleasedException ex)
+            {
+                MessageBox.Show(ex.Message, "Warning", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                e.Cancel = true;
+            }
+        }
+
+        private void btnON_Click(object sender, EventArgs e)
+        {
+            // Connect to the controller hardware and initialize it.
+            this.m_Stage.Initialize();
+
+            // Initialize stage control and update status indicator only if INIT worked.
+            if (this.m_Stage.IsInitialized)
+            {
+                // We cannot turn the stage on twice!
+                this.btnON.Enabled = false;
+                // Feedback to UI.
+                this.lblSTATUS.ForeColor = Color.FromKnownColor(KnownColor.Lime);
+                // We might want to turn it off.
+                this.btnOFF.Enabled = true;
+            }
+            else
+            {
+                this.lblSTATUS.ForeColor = Color.FromKnownColor(KnownColor.Red);
+            }
+
+            // The text indicating stage status should always be updated.
+            this.lblSTATUS.Text = this.m_Stage.IsInitialized.ToString();
+
+            // Update the UI.
+            this.UpdateUI();
+        }
+
+        private void btnOFF_Click(object sender, EventArgs e)
+        {
+            // Disconnect from the stage hardware.
+            this.m_Stage.Release();
+
+            // Release stage control and update status.
+            if (!this.m_Stage.IsInitialized)
+            {
+                this.btnOFF.Enabled = false;
+                this.lblSTATUS.ForeColor = Color.FromKnownColor(KnownColor.Red);
+                this.lblSTATUS.Text = m_Stage.IsInitialized.ToString();
+                this.btnON.Enabled = true;
+            }
+
+            // Update the UI.
+            UpdateUI();
+        }
+
+        private void btnMOVE_Click(object sender, EventArgs e)
+        {
+            double[] _dXYCoordinates = new double[3];
+            _dXYCoordinates[0] = Convert.ToDouble(this.txtMOVE.Text);
+            _dXYCoordinates[1] = 0.0;
+            _dXYCoordinates[2] = 0.0;
+            this.workerMove.RunWorkerAsync(_dXYCoordinates);
+        }
+
+        private void workerMove_DoWork(object sender, DoWorkEventArgs e)
+        {
+            double[] _dXYCoordinates = (double[])e.Argument;
+            this.m_Stage.MoveAbs(_dXYCoordinates[0], _dXYCoordinates[1], _dXYCoordinates[2]);
+        }
+
+        private void btnSTART_Click(object sender, EventArgs e)
+        {
+            Scanmode Scan = new UniDirXYScan(
+                Convert.ToUInt16(this.txtPIXELS.Text),
+                Convert.ToUInt16(this.txtPIXELS.Text),
+                0,
+                0,
+                0,
+                0,
+                1000.0,
+                1000.0,
+                1000.0,
+                25,
+                1,
+                1,
+                0.2);
+
+            this.m_frmTrajectoryForm.Visible = true;
+            this.m_frmTrajectoryForm.NMCoordinates = Scan.ScanCoordinates;
+
+            PrepnRun(Scan);
+        }
+
+        private void btnSTOP_Click(object sender, EventArgs e)
+        {
+            // Cancel de backgroundworker.
+            workerScan.CancelAsync();
+
+            // Enable all controls again.
+            EnableCtrls();
+
+            // Disable the Stop button again.
+            this.btnSTOP.Enabled = false;
+            this.btnSTART.Enabled = true;
+        }
+
+        private void PrepnRun(Scanmode __scnmScan)
+        {
+            // Disable the controls so the user cannot interfere with the scan. Only stopping the scan will be allowed.
+            DisableCtrls();
+            
+            // Check if the stage is definitely engaged and ready.... if not all other operations would be useless!
+            if (m_Stage.IsInitialized)
+            {
+                // Make sure the Stop button works.
+                this.btnSTOP.Enabled = true;
+
+                // Run the actual measurement in a separate thread to the UI thread. This will prevent the UI from blocking and it will
+                // enable continuous updates of the UI with scan data.
+                workerScan.RunWorkerAsync(__scnmScan);
+            }
+
+            // Update the UI.
+            UpdateUI();
+        }
+
+        private void workerScan_DoWork(object sender, DoWorkEventArgs e)
+        {
+            // Boolean value to indicate wheter or not the running scan should be stopped.
+            bool _bStop = false;
+
+            // Assign the values to be written. They were passed as an event argument to the DoWork event for the background worker.
+            Scanmode _Scan = (Scanmode)e.Argument;
+
+            // Initiate stage scan movement.
+            this.m_Stage.Scan(
+                _Scan,
+                Convert.ToDouble(this.txtTPL) / Convert.ToDouble(this.txtPIXELS),
+                true,
+                0.0,
+                0,
+                false,
+                0.0,
+                false);
+
+            while (!_bStop)
+            {
+                // Update the UI every 0.1 seconds, more than fast enough.
+                Thread.Sleep(100);
+
+                Application.DoEvents();
+
+                // Update the UI.
+                if (InvokeRequired)
+                {
+                    // Update the rest of the UI.
+                    Invoke(new UIUpdateDelegate(UpdateUI));
+                }
+
+                // Check if the worker was not cancelled.
+                if (workerScan.CancellationPending)
+                {
+                    e.Cancel = true;
+                    _bStop = true;
+                }
+            }
+
+            // Update the UI.
+            if (InvokeRequired)
+            {
+                // Update the rest of the UI.
+                Invoke(new UIUpdateDelegate(UpdateUI));
+            }
+        }
+    }
+}
